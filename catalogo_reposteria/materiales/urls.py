fbd922be6a2afdfea92ae1fe2f0378fc1c5b6177
SaiMatiX <matias.gallardo49@inacapmail.cl>
2026-09-04 17:52:20 -0400

    Delete catalogo_reposteria directory

--- catalogo_reposteria/materiales/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'materiales'
-
-urlpatterns = [
-    path('', views.lista, name='lista'),
-]
--- catalogo_reposteria/proveedores/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'proveedores'
-
-urlpatterns = [
-    path('', views.lista, name='lista'),
-]
